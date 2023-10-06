53c47a17d4e2aa1d44ffd831a5f5305e1834848b
jou.oonishi <joeoonishi@gmail.com>
2023-10-06 19:10:08 +0900

    instagram上への push

--- Instagram/SceneDelegate.swift
+++ Instagram/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  Instagram
-//
-//  Created by 大西穣 on 2023/09/10.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -17,6 +10,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        // SVProgressHUDをXcode11以上で実行するための環境調整コード --- ここから
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                appDelegate.window = self.window
+                // --- ここまで追加
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
